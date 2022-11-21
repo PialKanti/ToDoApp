c6304262d4cdf0a85d829282f56a08b5464b9666
PialKanti <pialkanti2012@gmail.com>
2022-11-21 16:24:10 +0600

    Updated todo item card design

--- ToDoApp/clientapp/src/components/ToDoItemCard.vue
+++ ToDoApp/clientapp/src/components/ToDoItemCard.vue
@@ -1,21 +1,81 @@
 <template>
-    {{ToDoItem}}
-    <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">{{todoItem.name}}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">{{todoItem.createdDateTime}}</h6>
-    <p class="card-text">{{todoItem.description}}</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+  {{ ToDoItem }}
+  <div class="card" style="width: 22rem">
+    <div class="card-header">
+      <h5>{{ todoItem.name }}</h5>
+    </div>
+    <div class="card-body">
+      <h6 class="card-subtitle mb-2 text-muted">
+        <font-awesome-icon icon="fa-regular fa-calendar-days" />
+        {{ getFormattedDateTime(todoItem.expiryDateTime) }}
+      </h6>
+      <p class="card-text">{{ todoItem.description }}</p>
+      <p>
+        <font-awesome-icon icon="fa-solid fa-location-dot" />
+        {{ todoItem.place }}
+      </p>
+      <div class="row">
+        <div class="col-md-4">
+          <button type="button" class="btn btn-light">
+            <font-awesome-icon icon="fa-regular fa-circle-check" />
+            Complete
+          </button>
+        </div>
+        <div class="col-md-4">
+          <button type="button" class="btn btn-light">
+            <font-awesome-icon icon="fa-regular fa-pen-to-square" />
+            Update
+          </button>
+        </div>
+        <div class="col-md-4">
+          <button type="button" class="btn btn-light">
+            <font-awesome-icon icon="fa-regular fa-trash-can" />
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-export default{
-    name : 'ToDoItemCard',
-    props:{
-        todoItem : Object
-    }
-}
+export default {
+  name: "ToDoItemCard",
+  props: {
+    todoItem: Object,
+  },
+  methods: {
+    getFormattedDateTime(dateTime) {
+      const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+
+      var date = new Date(dateTime);
+      return (
+        date.getDate() +
+        " " +
+        monthNames[date.getMonth()] +
+        ", " +
+        date.getFullYear() +
+        " " +
+        date.getHours() +
+        ":" +
+        date.getMinutes()
+      );
+    },
+  },
+};
 </script>
+
+<style scoped></style>
--- ToDoApp/clientapp/src/main.js
+++ ToDoApp/clientapp/src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import {faCalendarDays, faCheckCircle, faPenToSquare, faTrashCan} from '@fortawesome/free-regular-svg-icons'
+
+/* add icons to the library */
+library.add(faLocationDot, faCalendarDays, faCheckCircle, faPenToSquare, faTrashCan)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
